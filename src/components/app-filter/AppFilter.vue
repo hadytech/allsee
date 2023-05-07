c0e13f566ea32c49a7db760a9815245a9a5cbae4
hdayatech <intergo755@gmail.com>
2023-05-07 20:34:18 +0500

    add classes

---
+++ src/components/app-filter/AppFilter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="btn-group">
+      <button class="btn btn-dark" type="button">Barcha kinolar</button>
+      <button class="btn outline-dark" type="button">Yoqqan kinolar</button>
+      <button class="btn outline-dark" type="button">Mashhur kinolar</button>
+  </div>
+</template>
+
+<script>
+ export default{}
+</script>
+
+<style scoped> </style>
